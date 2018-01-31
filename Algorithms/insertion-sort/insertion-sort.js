07525ff01bb76aa5175e0f97435429bff378f649
Steve Meads <smeads@sofi.org>
2018-01-30 17:46:53 -0700

    Update insertion sort solution

--- Algorithms/insertion-sort/insertion-sort.js
+++ Algorithms/insertion-sort/insertion-sort.js
@@ -3,11 +3,28 @@
 /*
 Pseudocode:
 for j = 2 to A.length
-    key = A[j]
+    temp = A[j]
     - Insert A[j] into the sorted sequence A[1..j-1].
     i = j-1
-    while i > - and A[i] > key
+    while i > - and A[i] > temp
         A[i + 1] = A[i]
         i = i - 1
-    A[i + 1] = key
+    A[i + 1] = temp
 */ 
+
+const array = [5, 2, 4, 6, 1, 3];
+
+const insertionSort = (array) => {
+  for (let j = 0; j < array.length; j += 1) {
+    let temp = array[j];
+    let i = j - 1;
+    while(i >= 0 && array[i] > temp) {
+      array[i + 1] = array[i];
+      i = i - 1;
+    }
+    array[i + 1] = temp;
+  }
+  return array;
+}
+
+console.log(insertionSort(array));
